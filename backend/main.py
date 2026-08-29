1477d46aae63e011fe5f47d79be2ef56a70c846f
ivy1o1 <gupta.swastika2k7@gmail.com>
2026-08-30 01:18:05 +0530

    Added resume pydantic models

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import fitz
 from fastapi import FastAPI,UploadFile
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
 app = FastAPI()
 
 @app.get("/")
@@ -31,3 +33,50 @@ def create_user(user:User):
     return [
         {"message":f"User {user.name} received."}
     ]
+
+
+class Education(BAseModel):
+    degree:str
+    institution: str
+    major = str
+    start_date: str
+    end_date: str
+    gpa: str
+
+class Experience(BaseModel):
+    role: str
+    company: str
+    start_date: str
+    end_date: str
+    description: str
+
+class Project(BaseModel):
+    name: str
+    description: str
+    technologies: str
+
+class Certifications(BaseModel):
+    name: str
+    issuer: str
+    date: str
+
+class Achievements(BaseModel):
+    title: str
+    description: str
+
+class PersonalInfo(BaseModel):
+    name: str
+    email: str
+    phone: str
+
+class Resume(BaseModel):
+    personal_info: PersonalInfo
+    educaion: list[Education]
+    skills: list[str]
+    experience:list[Experience]
+    projects: list[Project]
+    certifications: list[Certifications]
+    achievements: list[Achievements]
+
+load_dotenv()
+api_key=os.getenv("GEMINI_API_KEY")
